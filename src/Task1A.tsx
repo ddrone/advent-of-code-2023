ac1d4932aba7e32e98714d330d2c27aee39dca61
Andrew Shulaev <ash.drone@gmail.com>
2023-12-02 14:48:37 +0000

    Second attempt at 1B

--- src/Task1A.tsx
+++ src/Task1A.tsx
@@ -22,13 +22,68 @@ function processLine(input: string): number {
   return firstDigit * 10 + lastDigit;
 }
 
+interface Digit {
+  digit: string;
+  location: number;
+}
+
+function findFirstAndLastDigits(input: string): [Digit, Digit] {
+  let firstDigit: Digit|undefined;
+  let lastDigit: Digit|undefined;
+
+  for (let i = 0; i < input.length; i++) {
+    if (!/[0-9]/.test(input.charAt(i))) {
+      continue;
+    }
+
+    const digit = {
+      digit: input[i],
+      location: i
+    }
+    if (firstDigit === undefined) {
+      firstDigit = digit;
+    }
+    lastDigit = digit;
+  }
+
+  if (firstDigit === undefined || lastDigit === undefined) {
+    throw new Error('no digits found!');
+  }
+
+  return [firstDigit, lastDigit];
+}
+
 const digits = 'one 1 two 2 three 3 four 4 five 5 six 6 seven 7 eight 8 nine 9'.split(' ');
 
-function preprocessLine(input: string): string {
-  for (let i = 0; i < digits.length; i += 2) {
-    input = input.replace(new RegExp(digits[i], 'g'), digits[i + 1]);
+function digitNum(n: string): number {
+  if (!/^[0-9]$/.test(n)) {
+    throw new Error(`${n} is not a digit!`);
   }
-  return input;
+
+  return n.charCodeAt(0) - '0'.charCodeAt(0);
+}
+
+function solve1BLine(input: string): number {
+  let [firstDigit, lastDigit] = findFirstAndLastDigits(input);
+  for (let i = 0; i < digits.length; i += 2) {
+    const firstOcc = input.indexOf(digits[i]);
+    if (firstOcc !== -1 && firstOcc < firstDigit.location) {
+      firstDigit = {
+        digit: digits[i + 1],
+        location: firstOcc
+      }
+
+      const lastOcc = input.lastIndexOf(digits[i]);
+      if (lastDigit.location < lastOcc) {
+        lastDigit = {
+          digit: digits[i + 1],
+          location: lastOcc
+        }
+      }
+    }
+  }
+
+  return digitNum(firstDigit.digit) * 10 + digitNum(lastDigit.digit);
 }
 
 export function solve1A(input: string): string {
@@ -38,9 +93,7 @@ export function solve1A(input: string): string {
 }
 
 export function solve1B(input: string): string {
-  const inputs = input.split('\n').map(preprocessLine);
-  const results = inputs.map(processLine);
-  console.log({inputs, results});
-  const sum = results.reduce((x, y) => x + y);
+  const inputs = input.split('\n').map(solve1BLine);
+  const sum = inputs.reduce((x, y) => x + y);
   return `${sum}`;
 }
