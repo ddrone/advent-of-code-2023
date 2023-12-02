bf7c1d570e3c1ae088c8b6a9090e0fab1d8c0d94
Andrew Shulaev <ash.drone@gmail.com>
2023-12-02 14:34:37 +0000

    First attempt at task 1B

--- src/Task1A.tsx
+++ src/Task1A.tsx
@@ -22,8 +22,25 @@ function processLine(input: string): number {
   return firstDigit * 10 + lastDigit;
 }
 
+const digits = 'one 1 two 2 three 3 four 4 five 5 six 6 seven 7 eight 8 nine 9'.split(' ');
+
+function preprocessLine(input: string): string {
+  for (let i = 0; i < digits.length; i += 2) {
+    input = input.replace(new RegExp(digits[i], 'g'), digits[i + 1]);
+  }
+  return input;
+}
+
 export function solve1A(input: string): string {
   const results = input.split('\n').map(processLine);
+  const sum = results.reduce((x, y) => x + y);
+  return `${sum}`;
+}
+
+export function solve1B(input: string): string {
+  const inputs = input.split('\n').map(preprocessLine);
+  const results = inputs.map(processLine);
+  console.log({inputs, results});
   const sum = results.reduce((x, y) => x + y);
   return `${sum}`;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { solve1A } from './Task1A.tsx'
+import { solve1B } from './Task1A.tsx'
 import Interactor from './Interactor.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Interactor process={solve1A} />
+    <Interactor process={solve1B} />
   </React.StrictMode>,
 )
