596fbd8aad73a3c620e53ffbf12bdce6354ced56
Andrew Shulaev <ash.drone@gmail.com>
2023-12-03 11:15:57 +0000

    Solve 3B

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Interactor from './Interactor.tsx'
-import { solve3A } from './task3.ts'
+import { solve3B } from './task3.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Interactor process={solve3A} />
+    <Interactor process={solve3B} />
   </React.StrictMode>,
 )
--- src/task3.ts
+++ src/task3.ts
@@ -40,6 +40,44 @@ function hasSymbol(grid: string[], rowIndex: number, colIndex: number) {
   return !(/[0-9.]/.test(char));
 }
 
+function hasGear(grid: string[], rowIndex: number, colIndex: number): boolean {
+  if (rowIndex < 0 || rowIndex >= grid.length) {
+    return false;
+  }
+
+  const row = grid[rowIndex];
+  if (colIndex < 0 || colIndex >= row.length) {
+    return false;
+  }
+
+  return row[colIndex] === '*';
+}
+
+interface GridPos {
+  rowIndex: number;
+  colIndex: number;
+}
+
+function findGears(n: GridNumber, grid: string[]): GridPos[] {
+  const result: GridPos[] = [];
+  function test(rowIndex: number, colIndex: number) {
+    if (hasGear(grid, rowIndex, colIndex)) {
+      result.push({
+        rowIndex, colIndex
+      });
+    }
+  }
+
+  for (let colIndex = n.colIndex - 1; colIndex <= n.colIndex + n.length; colIndex++) {
+    test(n.rowIndex - 1, colIndex);
+    test(n.rowIndex + 1, colIndex);
+  }
+
+  test(n.rowIndex, n.colIndex - 1);
+  test(n.rowIndex, n.colIndex + n.length);
+  return result;
+}
+
 function isPart(n: GridNumber, grid: string[]): boolean {
   for (let colIndex = n.colIndex - 1; colIndex <= n.colIndex + n.length; colIndex++) {
     if (hasSymbol(grid, n.rowIndex - 1, colIndex) || hasSymbol(grid, n.rowIndex + 1, colIndex)) {
@@ -53,4 +91,25 @@ function isPart(n: GridNumber, grid: string[]): boolean {
 export function solve3A(input: string): number {
   const grid = input.split('\n');
   return grid.map(findNumbers).reduce(concatArrays).filter(n => isPart(n, grid)).map(n => n.number).reduce(add);
+}
+
+export function solve3B(input: string): number {
+  const grid = input.split('\n');
+  const numbers = grid.map(findNumbers).reduce(concatArrays);
+  const gearMap = new Map<string, GridNumber[]>();
+
+  for (const n of numbers) {
+    for (const gear of findGears(n, grid)) {
+      const gridKey = `${gear.rowIndex},${gear.colIndex}`;
+      const adjNumbers = gearMap.get(gridKey);
+      if (adjNumbers === undefined) {
+        gearMap.set(gridKey, [n]);
+      }
+      else {
+        adjNumbers.push(n);
+      }
+    }
+  }
+
+  return [...gearMap.values()].filter(a => a.length === 2).map(a => a[0].number * a[1].number).reduce(add);
 }
